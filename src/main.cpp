2058df7f4f7f48fdac2769f6d63ac65091d9dad4
Wojciech Opara <wwojtek660@gmail.com>
2024-06-03 23:09:16 +0200

    Fix gcc compilation issues

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,13 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <span>
 #include <string_view>
 
-#include "gl/vertex_array.h"
-#include "gl/vertex_buffer.h"
-#include "gl/index_buffer.h"
-#include "types.h"
+#include "gl/vertex_array.hpp"
+#include "gl/vertex_buffer.hpp"
+#include "gl/index_buffer.hpp"
+#include "types.hpp"
 
 static constexpr std::string_view window_title = "Example";
 static constexpr int window_width = 800;
@@ -140,14 +141,14 @@ int main()
          -0.5f, -0.5f,
     };
 
-    u16 indices[] = {
+    GLushort indices[] = {
         0, 1, 2,
         0, 2, 3,
     };
 
     VertexArray va;
-    VertexBuffer vb(std::span{ vertices }, GL_STATIC_DRAW);
-    IndexBuffer ib(std::span{ indices }, GL_STATIC_DRAW);
+    VertexBuffer vb(std::span<GLfloat>{ vertices }, GL_STATIC_DRAW);
+    IndexBuffer ib(std::span<GLushort>{ indices }, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
