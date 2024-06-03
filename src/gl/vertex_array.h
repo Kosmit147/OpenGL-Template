4a9b104baa2ee636e3c3d6109e6c350d681470ba
Wojciech Opara <wwojtek660@gmail.com>
2024-06-03 17:02:21 +0200

    Implement VertexArray class

---
+++ src/gl/vertex_array.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <glad/glad.h>
+
+class VertexArray
+{
+    // TODO: copy / move semantics
+public:
+    explicit inline VertexArray() { glGenVertexArrays(1, &_id); }
+    inline ~VertexArray() { glDeleteVertexArrays(1, &_id); }
+
+    inline auto bind() const -> void { glBindVertexArray(_id); }
+    [[nodiscard]] inline auto id() const noexcept -> GLuint { return _id; }
+
+private: 
+    GLuint _id;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -4,54 +4,59 @@
 #include <iostream>
 #include <string_view>
 
+#include "types.h"
+#include "gl/vertex_array.h"
+
 static constexpr std::string_view window_title = "Example";
 static constexpr int window_width = 800;
 static constexpr int window_height = 600;
 
 int main()
 {
-    if (!glfwInit())
-    {
-        std::cerr << "Failed to initialize GLFW." << std::endl;
-        return -1;
-    }
+	if (!glfwInit())
+	{
+		std::cerr << "Failed to initialize GLFW." << std::endl;
+		return -1;
+	}
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(window_width, window_height, window_title.data(), NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(window_width, window_height, window_title.data(), NULL, NULL);
 
-    if (!window)
-    {
-        std::cerr << "Failed to create a window." << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+	if (!window)
+	{
+		std::cerr << "Failed to create a window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
-    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
-    {
-        std::cerr << "Failed to initialize GLAD." << std::endl;
-        glfwTerminate();
-        return -1;
-    }
+	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+	{
+		std::cerr << "Failed to initialize GLAD." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
-    glViewport(0, 0, window_width, window_height);
+	std::cout << glGetString(GL_VERSION) << std::endl;
+	glViewport(0, 0, window_width, window_height);
+
+	VertexArray vao;
 
 	while (!glfwWindowShouldClose(window))
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw here
+		// Draw here
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
-    glfwTerminate();
+	glfwTerminate();
 }
